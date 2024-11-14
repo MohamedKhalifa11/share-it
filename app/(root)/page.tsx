7075cb1cf7a5d55c565dd17beb272a961ba26705
Mohamed Khalifa <mkhkun11@gmail.com>
2024-11-14 09:22:42 +0200

    Add Search Form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,21 @@
-export default function Home() {
-  console.log("Server or Client");
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  console.log(searchParams);
+  console.log(query);
+
   return (
     <section className="blue_container">
       <h1 className="heading">Share Your Tech Insights with the World</h1>
       <p className="sub-heading !max-w-3xl">
         Explore the latest in tech, or start a conversation with your own blog.
       </p>
+      <SearchForm query={query} />
     </section>
   );
 }
