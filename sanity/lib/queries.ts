908c3ee9357cea75795c4b744963065483b380d6
Mohamed Khalifa <mkhkun11@gmail.com>
2024-12-15 06:45:14 +0200

    add playlist

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,6 +1,6 @@
 import { defineQuery } from "next-sanity";
 
-export const BLOGS_QUERY = defineQuery( 
+export const BLOGS_QUERY = defineQuery(
   `*[_type == "blog" && defined(slug.current) && (!defined($search) || title match "*"+$search+"*" || category match "*"+$search+"*" || author->name match "*"+$search+"*")] | order(_createdAt desc) {
   _id, 
   title, 
@@ -11,8 +11,8 @@ export const BLOGS_QUERY = defineQuery(
   description, 
   category, 
   image
-}`
-)
+}`,
+);
 
 export const BLOGS_BY_ID_QUERY = defineQuery(
   `*[_type == "blog" && _id == $id][0]{
@@ -26,11 +26,11 @@ export const BLOGS_BY_ID_QUERY = defineQuery(
   category, 
   image,
   pitch
-}`
-)
+}`,
+);
 
 export const BLOG_VIEWS_QUERY = defineQuery(
-  `*[_type == "blog" && _id == $id][0]{ _id, views }`
+  `*[_type == "blog" && _id == $id][0]{ _id, views }`,
 );
 
 export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(
@@ -42,7 +42,7 @@ export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(
       email,
       image,
       bio
-  }`
+  }`,
 );
 
 export const AUTHOR_BY_ID_QUERY = defineQuery(
@@ -54,10 +54,10 @@ export const AUTHOR_BY_ID_QUERY = defineQuery(
       email,
       image,
       bio
-  }`
+  }`,
 );
 
-export const BLOGS_BY_AUTHOR_QUERY = defineQuery( 
+export const BLOGS_BY_AUTHOR_QUERY = defineQuery(
   `*[_type == "blog" && author._ref == $id] | order(_createdAt desc) {
   _id, 
   title, 
@@ -68,5 +68,31 @@ export const BLOGS_BY_AUTHOR_QUERY = defineQuery(
   description, 
   category, 
   image
-}`
-)
+}`,
+);
+
+// export const PLAYLIST_BY_SLUG_QUERY = defineQuery(
+//   `*[_type == "playlist" && slug.current == $slug][0]{
+//   _id,
+//   title,
+//   slug,
+//   select[]->{
+//     _id,
+//     _createdAt,
+//     title,
+//     slug,
+//     author->{
+//       _id,
+//       name,
+//       slug,
+//       image,
+//       bio
+//     },
+//     views,
+//     description,
+//     category,
+//     image,
+//     pitch
+//   }
+// }`,
+// );
